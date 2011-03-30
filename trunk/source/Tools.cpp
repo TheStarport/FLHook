ee2753e3fffb9324b3134a194eada11518b29a76
cannon <cannon@459373cf-9a4d-42fe-bae1-5ac7ab2b8a38>
2011-03-30 06:52:47 +0000

    Added stack trace on exception.

--- trunk/source/Tools.cpp
+++ trunk/source/Tools.cpp
@@ -352,61 +352,76 @@ HMODULE GetModuleAddr(uint iAddr)
 	return 0;
 }
 
+#include "StackWalker.h"
+
+class StackWalkerToLog : public StackWalker
+{
+protected:
+	virtual void OnCallstackEntry(CallstackEntryType eType, CallstackEntry &entry)
+	{
+		if (entry.offset)
+		{
+			if (entry.lineFileName[0])
+			{
+				AddLog("Offset=%x Module=\"%s\" File=\"%s:%d\" Func=\"%s\"", (uint)entry.offset, entry.moduleName, entry.lineFileName, entry.lineNumber, entry.name);
+			}
+			else if (entry.name)
+			{
+				AddLog("Offset=%x Module=\"%s\" Func=\"%s\"", (uint)entry.offset, entry.moduleName, entry.name);
+			}
+			else
+			{
+				AddLog("Offset=%x Module=\"%s\"", (uint)entry.offset, entry.moduleName);
+			}
+		}
+	}
+	virtual void OnDbgHelpErr(LPCSTR szFuncName, DWORD gle, DWORD64 addr)
+	{
+		// suppress this
+	}
+	virtual void OnOutput(LPCSTR szText)
+	{
+		// suppress this
+	}
+};
+
 void AddExceptionInfoLog()
 {
 	try{
 		EXCEPTION_RECORD const *exception = GetCurrentExceptionRecord();
 		_CONTEXT const *reg = GetCurrentExceptionContext();
 
-		if(exception)
+		if (exception)
 		{
 			DWORD iCode = exception->ExceptionCode;
 			uint iAddr = (uint)exception->ExceptionAddress;
 			uint iOffset = 0;
 			HMODULE hModExc = GetModuleAddr(iAddr);
 			char szModName[MAX_PATH] = "";
-			if(hModExc)
+			if (hModExc)
 			{
 				iOffset = iAddr - (uint)hModExc;
 				GetModuleFileName(hModExc, szModName, sizeof(szModName));
 			}
 			AddLog("Code=%x Offset=%x Module=\"%s\"", iCode, iOffset, szModName);
-			if(iCode == 0xE06D7363 && exception->NumberParameters == 3) //C++ exception
-			{
-				_s__ThrowInfo *info = (_s__ThrowInfo*)exception->ExceptionInformation[2];
-				const _s__CatchableType *const (*typeArr)[] = &info->pCatchableTypeArray->arrayOfCatchableTypes;
-				std::exception *obj = (std::exception*)exception->ExceptionInformation[1];
-				const char *szName = info && info->pCatchableTypeArray && info->pCatchableTypeArray->nCatchableTypes ? (*typeArr)[0]->pType->name : "";
-				int i = 0;
-				for(; i < info->pCatchableTypeArray->nCatchableTypes; i++)
-				{
-					if(!strcmp(".?AVexception@@", (*typeArr)[i]->pType->name))
-						break;
-				}
-				const char *szMessage = i != info->pCatchableTypeArray->nCatchableTypes ? obj->what() : "";
-				//C++ exceptions are triggered by RaiseException in kernel32, so use ebp to get the return address
-				iOffset = 0;
-				szModName[0] = 0;
-				if(reg)
-				{
-					iAddr = *((*((uint**)reg->Ebp)) + 1);
-					hModExc = GetModuleAddr(iAddr);
-					if(hModExc)
-					{
-						iOffset = iAddr - (uint)hModExc;
-						GetModuleFileName(hModExc, szModName, sizeof(szModName));
-					}
-				}
-				AddLog("Name=\"%s\" Message=\"%s\" Offset=%x Module=\"%s\"", szName, szMessage, iOffset, strrchr(szModName, '\\')+1);
-			}	
 		}
 		else
+		{
 			AddLog("No exception information available");
-		if(reg)
+		}
+
+		if (reg)
+		{
 			AddLog("eax=%x ebx=%x ecx=%x edx=%x edi=%x esi=%x ebp=%x eip=%x esp=%x",
 				reg->Eax, reg->Ebx, reg->Ecx, reg->Edx, reg->Edi, reg->Esi, reg->Ebp, reg->Eip, reg->Esp);
+
+			StackWalkerToLog sw;
+			sw.ShowCallstack(GetCurrentThread(), reg);
+		}
 		else
+		{
 			AddLog("No register information available");
+		}
 	} catch(...) { AddLog("Exception in AddExceptionInfoLog!"); }
 }
 
