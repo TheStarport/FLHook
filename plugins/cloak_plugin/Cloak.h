722fe1b9f762c2fda88cd269b85314aae4fdc300
cannon <cannon@459373cf-9a4d-42fe-bae1-5ac7ab2b8a38>
2012-08-18 02:42:47 +0000

    Player ship cloaking plugin commit

---
+++ plugins/cloak_plugin/Cloak.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__ 1
+
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <math.h>
+#include <list>
+#include <map>
+#include <algorithm>
+#include "./headers/FLHook.h"
+#include "./headers/plugin.h"
+#include "PluginUtilities.h"
+#include <math.h>
+using namespace std;
+
+#endif
