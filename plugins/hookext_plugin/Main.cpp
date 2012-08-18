c0fdf1f654c3798795d26ea3ca5031034c18ae56
cannon <cannon@459373cf-9a4d-42fe-bae1-5ac7ab2b8a38>
2012-08-18 02:40:35 +0000

    Added hook extension plugin to extend character file writing.

---
+++ plugins/hookext_plugin/Main.cpp
@@ -0,0 +1,363 @@
+/**
+HookExtension Plugin by Cannon
+*/
+
+// includes 
+
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <math.h>
+#include <list>
+#include <map>
+#include <algorithm>
+#include "./headers/FLHook.h"
+#include "./headers/plugin.h"
+#include <math.h>
+
+struct FLHOOK_PLAYER_DATA
+{
+	string charfilename;
+	map<string, string> lines;
+};
+
+map<uint, FLHOOK_PLAYER_DATA> clients;
+
+/// A return code to indicate to FLHook if we want the hook processing to continue.
+PLUGIN_RETURNCODE returncode;
+
+string GetAccountDir(uint client)
+{
+	static _GetFLName GetFLName = (_GetFLName)((char*)hModServer + 0x66370);
+	char dirname[1024];
+	GetFLName(dirname, Players[client].Account->wszAccID);
+	return dirname;
+}
+
+string GetCharfilename(const wstring &charname)
+{
+	static _GetFLName GetFLName = (_GetFLName)((char*)hModServer + 0x66370);
+	char filename[1024];
+	GetFLName(filename, charname.c_str());
+	return filename;
+}
+
+static PlayerData *CurrPlayer;
+int __stdcall HkCb_UpdateFile(char *filename, wchar_t *savetime, int b)
+{
+	// Call the original save charfile function
+	int retv;
+	__asm
+	{
+		pushad
+		mov ecx, [CurrPlayer]
+		push b
+		push savetime
+		push filename
+		mov eax, 0x6d4ccd0
+		call eax
+		mov retv, eax
+		popad
+	}
+
+	// Readd the flhook section.
+	if (retv)
+	{
+		uint client = CurrPlayer->iOnlineID;
+
+		string path = scAcctPath + GetAccountDir(client) + "\\" + filename;
+		FILE *file = fopen(path.c_str(), "a");
+		if (file)
+		{
+			fprintf(file, "[flhook]\n");
+			for (map<string, string>::iterator i = clients[client].lines.begin();
+				i != clients[client].lines.end(); ++i)
+				fprintf(file, "%s = %s\n", i->first.c_str(), i->second.c_str());
+			fclose(file);
+		}
+	}
+
+	return retv; 
+}
+
+__declspec(naked)void HkCb_UpdateFileNaked()
+{
+	__asm mov CurrPlayer, ecx
+	__asm jmp HkCb_UpdateFile 
+}
+
+
+/// Clear client info when a client connects.
+void ClearClientInfo(uint client)
+{
+	returncode = DEFAULT_RETURNCODE;
+	clients.erase(client);
+}
+
+/// Load the flhook section from the character file
+void __stdcall CharacterSelect(struct CHARACTER_ID const &charid, unsigned int client)
+{
+	returncode = DEFAULT_RETURNCODE;
+
+	string path = scAcctPath + GetAccountDir(client) + "\\" + charid.szCharFilename;
+
+	//ConPrint(L"CharacterSelect=%s\n", stows(path).c_str());
+
+	clients[client].charfilename = charid.szCharFilename;
+	clients[client].lines.clear();
+
+	// Read the flhook section so that we can rewrite after the save so that it isn't lost
+	INI_Reader ini;
+	if (ini.open(path.c_str(), false))
+	{
+		while (ini.read_header())
+		{
+			if (ini.is_header("flhook"))
+			{
+				wstring tag;
+				while (ini.read_value())
+				{
+					clients[client].lines[ini.get_name_ptr()] = ini.get_value_string();
+				}
+			}
+		}
+		ini.close();
+	}
+}
+
+void __stdcall DisConnect(unsigned int client, enum EFLConnection p2)
+{
+	returncode = DEFAULT_RETURNCODE;
+	clients.erase(client);
+}
+
+/// Load the configuration
+void LoadSettings()
+{
+	returncode = DEFAULT_RETURNCODE;
+
+	// The path to the configuration file.
+	char szCurDir[MAX_PATH];
+	GetCurrentDirectory(sizeof(szCurDir), szCurDir);
+	string scPluginCfgFile = string(szCurDir) + "\\flhook_plugins\\hookext.cfg";
+
+	clients.clear();
+	struct PlayerData *pPD = 0;
+	while(pPD = Players.traverse_active(pPD))
+	{
+		uint client = HkGetClientIdFromPD(pPD);
+		wstring charname = Players.GetActiveCharacterName(client);
+		string filename = GetCharfilename(charname) + ".fl";
+		CHARACTER_ID charid;
+		strcpy(charid.szCharFilename, filename.c_str());
+		CharacterSelect(charid,client);
+	}
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	static bool patched = false;
+	srand((uint)time(0));
+
+	// If we're being loaded from the command line while FLHook is running then
+	// set_scCfgFile will not be empty so load the settings as FLHook only
+	// calls load settings on FLHook startup and .rehash.
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		if (set_scCfgFile.length()>0)
+			LoadSettings();
+
+		if (!patched)
+		{
+			patched = true;
+			PatchCallAddr((char*)hModServer, 0x6c547, (char*)HkCb_UpdateFileNaked);
+			PatchCallAddr((char*)hModServer, 0x6c9cd, (char*)HkCb_UpdateFileNaked);
+		}
+	}
+	else if (fdwReason == DLL_PROCESS_DETACH)
+	{
+		if (patched)
+		{
+			patched = false;
+			{
+				BYTE patch[] = { 0xE8, 0x84, 0x07, 0x00, 0x00 };
+				WriteProcMem((char*)hModServer + 0x6c547, patch, 5);
+			}
+
+			{
+				BYTE patch[] = { 0xE8, 0xFE, 0x2, 0x00, 0x00 };
+				WriteProcMem((char*)hModServer + 0x6c9cd, patch, 5);
+			}
+		}
+	}
+	return true;
+}
+
+namespace HookExt
+{
+	EXPORT string IniGetS(uint client, const string &name)
+	{
+		if (clients.find(client) == clients.end())
+			return "";
+
+		if (!clients[client].charfilename.length())
+			return "";
+		
+		if (clients[client].lines.find(name)
+			== clients[client].lines.end())
+			return "";
+		
+		return clients[client].lines[name];
+	}
+
+	EXPORT wstring IniGetWS(uint client, const string &name)
+	{
+		string svalue = HookExt::IniGetS(client, name);
+
+		wstring value;
+		long lHiByte;
+		long lLoByte;
+		while(sscanf(svalue.c_str(), "%02X%02X", &lHiByte, &lLoByte) == 2)
+		{
+			svalue = svalue.substr(4);
+			wchar_t wChar = (wchar_t)((lHiByte << 8) | lLoByte);
+			value.append(1, wChar);
+		}
+		return value;
+	}
+
+	EXPORT uint IniGetI(uint client, const string &name)
+	{
+		string svalue = HookExt::IniGetS(client, name);
+		return strtoul(svalue.c_str(), 0, 10);
+	}
+
+	EXPORT bool IniGetB(uint client, const string &name)
+	{
+		string svalue = HookExt::IniGetS(client, name);
+		if (svalue == "yes")
+			return true;
+		return false;
+	}
+
+	EXPORT float IniGetF(uint client, const string &name)
+	{
+		string svalue = HookExt::IniGetS(client, name);
+		return (float)atof(svalue.c_str());
+	}
+
+	EXPORT void IniSetS(uint client, const string &name, const string &value)
+	{
+		clients[client].lines[name] = value;
+	}
+
+	EXPORT void IniSetWS(uint client, const string &name, const wstring &value)
+	{
+		string svalue = "";
+		for(uint i = 0; (i < value.length()); i++)
+		{
+			char cHiByte = value[i] >> 8;
+			char cLoByte = value[i] & 0xFF;
+			char szBuf[8];
+			sprintf(szBuf, "%02X%02X", ((uint)cHiByte) & 0xFF, ((uint)cLoByte) & 0xFF);
+			svalue += szBuf;
+		}
+
+		HookExt::IniSetS(client, name, svalue);
+	}
+
+	EXPORT void IniSetI(uint client, const string &name, uint value)
+	{
+		char svalue[100];
+		sprintf(svalue, "%u", value);
+		HookExt::IniSetS(client, name, svalue);
+	}
+
+	EXPORT void IniSetB(uint client, const string &name, bool value)
+	{
+		string svalue = value ? "yes" : "no";
+		HookExt::IniSetS(client, name, svalue);
+	}
+
+	EXPORT void IniSetF(uint client, const string &name, float value)
+	{
+		char svalue[100];
+		sprintf(svalue, "%0.02f", value);
+		HookExt::IniSetS(client, name, svalue);
+	}
+
+	EXPORT void IniSetS(const wstring &charname, const string &name, const string &value)
+	{
+		// If the player is online then update the in memory cache.
+		string charfilename = GetCharfilename(charname) + ".fl";
+		for (map<uint, FLHOOK_PLAYER_DATA>::iterator i = clients.begin(); i != clients.end(); ++i)
+		{
+			if (i->second.charfilename == charfilename)
+			{
+				HookExt::IniSetS(i->first, name, value);
+				return;
+			}
+		}
+
+		// Otherwise write directly to the character file if it exists.
+		CAccount *acc = HkGetAccountByCharname(charname);
+		if (acc)
+		{
+			string charpath = scAcctPath + GetCharfilename(acc->wszAccID) + "\\" + charfilename;
+			WritePrivateProfileString("flhook", name.c_str(), value.c_str(), charpath.c_str());
+		}		
+	}
+
+	EXPORT void IniSetWS(const wstring &charname, const string &name, const wstring &value)
+	{
+		string svalue = "";
+		for(uint i = 0; (i < value.length()); i++)
+		{
+			char cHiByte = value[i] >> 8;
+			char cLoByte = value[i] & 0xFF;
+			char szBuf[8];
+			sprintf(szBuf, "%02X%02X", ((uint)cHiByte) & 0xFF, ((uint)cLoByte) & 0xFF);
+			svalue += szBuf;
+		}
+
+		HookExt::IniSetS(charname, name, svalue);
+	}
+
+	EXPORT void IniSetI(const wstring &charname, const string &name, uint value)
+	{
+		char svalue[100];
+		sprintf(svalue, "%u", value);
+		HookExt::IniSetS(charname, name, svalue);
+	}
+
+	EXPORT void IniSetB(const wstring &charname, const string &name, bool value)
+	{
+		string svalue = value ? "yes" : "no";
+		HookExt::IniSetS(charname, name, svalue);
+	}
+
+	EXPORT void IniSetF(const wstring &charname, const string &name, float value)
+	{
+		char svalue[100];
+		sprintf(svalue, "%0.02f", value);
+		HookExt::IniSetS(charname, name, svalue);
+	}
+}
+
+/** Functions to hook */
+EXPORT PLUGIN_INFO* Get_PluginInfo()
+{
+	PLUGIN_INFO* p_PI = new PLUGIN_INFO();
+	p_PI->sName = "HookExt Plugin by Cannon";
+	p_PI->sShortName = "hookext";
+	p_PI->bMayPause = true;
+	p_PI->bMayUnload = true;
+	p_PI->ePluginReturnCode = &returncode;
+	p_PI->lstHooks.push_back(PLUGIN_HOOKINFO((FARPROC*)&LoadSettings, PLUGIN_LoadSettings, 0));
+	p_PI->lstHooks.push_back(PLUGIN_HOOKINFO((FARPROC*)&ClearClientInfo, PLUGIN_ClearClientInfo, 0));
+	p_PI->lstHooks.push_back(PLUGIN_HOOKINFO((FARPROC*)&CharacterSelect, PLUGIN_HkIServerImpl_CharacterSelect, 0));	
+	//p_PI->lstHooks.push_back(PLUGIN_HOOKINFO((FARPROC*)&DisConnect, PLUGIN_HkIServerImpl_DisConnect,0));
+	
+	return p_PI;
+}
