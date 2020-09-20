6318d5830df1453e5a858557bde6ba3cd3a06dd4
Raikkonen <40522943+ollieraikkonen@users.noreply.github.com>
2020-09-20 23:26:30 +0100

    PvP Plugin  (#19)
    * PvP Plugin
    
    /acceptduel - Usage: /acceptduel
    /acceptffa - Usage: /acceptffa
    /cancelbet - Usage: /cancel
    /duel - Usage: /duel <charname> <amount>
    /ffa - Usage: /ffa <amount>
    
    * PvP: Bug fixes
    
    Bug fixes. /ffa and /acceptffa work correctly now
    
    * PvP: Bug fixes and restructure
    
    * PvP: More formatting improvements
    
    * PvP: Remove unnecessary hook, make Cancel handle FFAs too
    
    * PvP: Check for valid ClientID on dock hook, also fix wrong HKGetCash call
    
    * PvP: More bug fixes + change /cancelbet to /cancel since it can cancel FFA entry too
    
    * PvP: Swap hook and bug fix
    
    * PvP: Stop plugin from being paused
    
    * PvP: Changed hook to correct one / renamed methods / processFFA no longer requires iSystem (what if the player F1ed out of the system)
    
    * PvP: Changed uints to int

---
+++ plugins/pvp_plugin/Main.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <math.h>
+#include <list>
+#include <map>
+#include <algorithm>
+#include <FLHook.h>
+#include <plugin.h>
+#include "PluginUtilities.h"
+
+PLUGIN_RETURNCODE returncode;
+
+typedef bool (*_UserCmdProc)(uint, const std::wstring&, const std::wstring&, const wchar_t*);
+
+struct USERCMD
+{
+	wchar_t* wszCmd;
+	_UserCmdProc proc;
+	wchar_t* usage;
+};
+
+#define IS_CMD(a) !wscCmd.compare(L##a)
+
+void AddExceptionInfoLog(struct SEHException* pep);
