e8b739d1b6fa74ace25f3e8e194d2ce6bd6b50e1
Raikkonen <40522943+ollieraikkonen@users.noreply.github.com>
2021-01-04 16:40:01 +0000

    Bounty Hunt Plugin (#23)
    * Bounty Hunt Plugin
    
    Originally by ||KOS||Acid
    
    Usage:
    - /bountyhunt <playername> <credits> <time>
    - /bountyhuntid <id> <credits> <time>
    
    * Bounty Hunt: Forgot return code and breaking out of the for loop after removing the iterator

---
+++ plugins/bountyhunt_plugin/Main.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <list>
+#include <FLHook.h>
+#include <plugin.h>
+
+static int set_iPluginDebug = 0;
+PLUGIN_RETURNCODE returncode;
+
+typedef bool (*_UserCmdProc)(uint, const std::wstring&, const std::wstring&, const wchar_t*);
+
+struct USERCMD
+{
+	wchar_t* wszCmd;
+	_UserCmdProc proc;
+	wchar_t* usage;
+};
+
+#define IS_CMD(a) !wscCmd.compare(L##a)
