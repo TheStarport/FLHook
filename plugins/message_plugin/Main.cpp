6947a524b23a902cad63e3b427b8dd7e8000769a
ollieraikkonen <olliezeta9@gmail.com>
2022-05-10 22:23:23 +0100

    Remove pointless death message, its in core!

--- plugins/message_plugin/Main.cpp
+++ plugins/message_plugin/Main.cpp
@@ -76,17 +76,6 @@ static void ShowStandardBanner() {
     }
 }
 
-static std::wstring SetSizeToSmall(const std::wstring &wscDataFormat) {
-    uint iFormat = wcstoul(wscDataFormat.c_str() + 2, 0, 16);
-    wchar_t wszStyleSmall[32];
-    wcscpy(wszStyleSmall, wscDataFormat.c_str());
-    swprintf(wszStyleSmall + wcslen(wszStyleSmall) - 2,
-             sizeof(wszStyleSmall) / sizeof(wchar_t) + wcslen(wszStyleSmall) -
-                 2,
-             L"%02X", 0x90 | (iFormat & 7));
-    return wszStyleSmall;
-}
-
 /** Replace #t and #c tags with current target name and current ship location.
 Return false if tags cannot be replaced. */
 static bool ReplaceMessageTags(uint iClientID, INFO &clientData,
@@ -968,116 +957,6 @@ void UserCmd_Do(uint iClientID, const std::wstring &wscParam) {
     }
 }
 
-/// Hook for ship distruction. It's easier to hook this than the PlayerDeath
-/// one. Drop a percentage of cargo + some loot representing ship bits.
-void SendDeathMsg(const std::wstring &wscMsg, uint iSystemID,
-                           uint iClientIDVictim, uint iClientIDKiller) {
-    // encode xml std::string(default and small)
-    // non-sys
-    std::wstring wscXMLMsg =
-        L"<TRA data=\"" + set_wscDeathMsgStyle + L"\" mask=\"-1\"/> <TEXT>";
-    wscXMLMsg += XMLText(wscMsg);
-    wscXMLMsg += L"</TEXT>";
-
-    char szBuf[0xFFFF];
-    uint iRet;
-    if (!HKHKSUCCESS(HkFMsgEncodeXML(wscXMLMsg, szBuf, sizeof(szBuf), iRet)))
-        return;
-
-    std::wstring wscStyleSmall = SetSizeToSmall(set_wscDeathMsgStyle);
-    std::wstring wscXMLMsgSmall = std::wstring(L"<TRA data=\"") +
-                                  wscStyleSmall + L"\" mask=\"-1\"/> <TEXT>";
-    wscXMLMsgSmall += XMLText(wscMsg);
-    wscXMLMsgSmall += L"</TEXT>";
-    char szBufSmall[0xFFFF];
-    uint iRetSmall;
-    if (!HKHKSUCCESS(HkFMsgEncodeXML(wscXMLMsgSmall, szBufSmall,
-                                     sizeof(szBufSmall), iRetSmall)))
-        return;
-
-    // sys
-    std::wstring wscXMLMsgSys =
-        L"<TRA data=\"" + set_wscDeathMsgStyleSys + L"\" mask=\"-1\"/> <TEXT>";
-    wscXMLMsgSys += XMLText(wscMsg);
-    wscXMLMsgSys += L"</TEXT>";
-    char szBufSys[0xFFFF];
-    uint iRetSys;
-    if (!HKHKSUCCESS(
-            HkFMsgEncodeXML(wscXMLMsgSys, szBufSys, sizeof(szBufSys), iRetSys)))
-        return;
-
-    std::wstring wscStyleSmallSys = SetSizeToSmall(set_wscDeathMsgStyleSys);
-    std::wstring wscXMLMsgSmallSys =
-        L"<TRA data=\"" + wscStyleSmallSys + L"\" mask=\"-1\"/> <TEXT>";
-    wscXMLMsgSmallSys += XMLText(wscMsg);
-    wscXMLMsgSmallSys += L"</TEXT>";
-    char szBufSmallSys[0xFFFF];
-    uint iRetSmallSys;
-    if (!HKHKSUCCESS(HkFMsgEncodeXML(wscXMLMsgSmallSys, szBufSmallSys,
-                                     sizeof(szBufSmallSys), iRetSmallSys)))
-        return;
-
-    // send
-    // for all players
-    struct PlayerData *pPD = 0;
-    while (pPD = Players.traverse_active(pPD)) {
-        uint iClientID = HkGetClientIdFromPD(pPD);
-        uint iClientSystemID = 0;
-        pub::Player::GetSystem(iClientID, iClientSystemID);
-
-        if (mapInfo[iClientID].bShowChatTime) {
-            // Send time with gray color (BEBEBE) in small text (90) above the
-            // chat line.
-            bSendingTime = true;
-            HkFMsg(iClientID, L"<TRA data=\"0xBEBEBE90\" mask=\"-1\"/><TEXT>" +
-                                  XMLText(GetTimeString(set_bLocalTime)) +
-                                  L"</TEXT>");
-            bSendingTime = false;
-        }
-
-        char *szXMLBuf;
-        int iXMLBufRet;
-        char *szXMLBufSys;
-        int iXMLBufRetSys;
-        if (set_bUserCmdSetDieMsgSize &&
-            (ClientInfo[iClientID].dieMsgSize == CS_SMALL)) {
-            szXMLBuf = szBufSmall;
-            iXMLBufRet = iRetSmall;
-            szXMLBufSys = szBufSmallSys;
-            iXMLBufRetSys = iRetSmallSys;
-        } else {
-            szXMLBuf = szBuf;
-            iXMLBufRet = iRet;
-            szXMLBufSys = szBufSys;
-            iXMLBufRetSys = iRetSys;
-        }
-
-        if (!set_bUserCmdSetDieMsg) { // /set diemsg disabled, thus send to all
-            if (iSystemID == iClientSystemID)
-                HkFMsgSendChat(iClientID, szXMLBufSys, iXMLBufRetSys);
-            else
-                HkFMsgSendChat(iClientID, szXMLBuf, iXMLBufRet);
-            continue;
-        }
-
-        if (ClientInfo[iClientID].dieMsg == DIEMSG_NONE)
-            continue;
-        else if ((ClientInfo[iClientID].dieMsg == DIEMSG_SYSTEM) &&
-                 (iSystemID == iClientSystemID))
-            HkFMsgSendChat(iClientID, szXMLBufSys, iXMLBufRetSys);
-        else if ((ClientInfo[iClientID].dieMsg == DIEMSG_SELF) &&
-                 ((iClientID == iClientIDVictim) ||
-                  (iClientID == iClientIDKiller)))
-            HkFMsgSendChat(iClientID, szXMLBufSys, iXMLBufRetSys);
-        else if (ClientInfo[iClientID].dieMsg == DIEMSG_ALL) {
-            if (iSystemID == iClientSystemID)
-                HkFMsgSendChat(iClientID, szXMLBufSys, iXMLBufRetSys);
-            else
-                HkFMsgSendChat(iClientID, szXMLBuf, iXMLBufRet);
-        }
-    }
-}
-
 // Client command processing
 USERCMD UserCmds[] = {
     {L"/setmsg", UserCmd_SetMsg},
@@ -1203,5 +1082,4 @@ extern "C" EXPORT void ExportPluginInfo(PluginInfo *pi) {
     pi->emplaceHook(HookedCall::IServerImpl__DisConnect, &DisConnect);
     pi->emplaceHook(HookedCall::FLHook__ClearClientInfo, &ClearClientInfo);
     pi->emplaceHook(HookedCall::IServerImpl__PlayerLaunch, &PlayerLaunch);
-    pi->emplaceHook(HookedCall::IEngine__SendDeathMessage, &SendDeathMsg);
 }
