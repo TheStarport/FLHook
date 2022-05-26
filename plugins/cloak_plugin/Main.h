08f44ae243df79bd07411469d8d44ceb30a05fc8
ollieraikkonen <olliezeta9@gmail.com>
2022-05-26 17:57:24 +0100

    Start porting Cloak plugin to serialization (not working due to serialization issue to be logged)

---
+++ plugins/cloak_plugin/Main.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <FLHook.h>
+#include <plugin.h>
+
+namespace Plugins::Cloak
+{
+
+	enum INFO_STATE
+	{
+		STATE_CLOAK_INVALID = 0,
+		STATE_CLOAK_OFF = 1,
+		STATE_CLOAK_CHARGING = 2,
+		STATE_CLOAK_ON = 3,
+	};
+
+	struct CLOAK_ARCH
+	{
+		std::string scNickName;
+		int iWarmupTime;
+		int iCooldownTime;
+		int iHoldSizeLimit;
+		std::map<uint, uint> mapFuelToUsage;
+		bool bDropShieldsOnUncloak;
+	};
+
+	struct CLOAK_INFO
+	{
+		CLOAK_INFO()
+		{
+			uint iCloakSlot = 0;
+			bCanCloak = false;
+			mstime tmCloakTime = 0;
+			uint iState = STATE_CLOAK_INVALID;
+			uint bAdmin = false;
+
+			arch.iWarmupTime = 0;
+			arch.iCooldownTime = 0;
+			arch.iHoldSizeLimit = 0;
+			arch.mapFuelToUsage.clear();
+			arch.bDropShieldsOnUncloak = false;
+		}
+
+		uint iCloakSlot;
+		bool bCanCloak;
+		mstime tmCloakTime;
+		uint iState;
+		bool bAdmin;
+
+		CLOAK_ARCH arch;
+	};
+
+	struct Config final : Reflectable
+	{
+		std::string File() override { return "flhook_plugins/cloak.json"; }
+
+		struct CLOAK_ARCH_REFLECTABLE : Reflectable
+		{
+			std::string scNickName;
+			int iWarmupTime;
+			int iCooldownTime;
+			int iHoldSizeLimit;
+			//std::map<std::string, int> mapFuelToUsage;
+			bool bDropShieldsOnUncloak;
+		};
+
+		// Reflectable fields
+		std::map<std::string, CLOAK_ARCH_REFLECTABLE> mapCloakingDevices;
+	};
+
+	//! Global data for this plugin
+	struct Global final
+	{
+		std::unique_ptr<Config> config = nullptr;
+
+		// Other fields
+		ReturnCode returncode = ReturnCode::Default;
+
+		std::map<uint, CLOAK_INFO> mapClientsCloak;
+		std::map<uint, CLOAK_ARCH> mapCloakingDevices;
+	};
+}
+
