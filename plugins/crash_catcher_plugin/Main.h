46f646b1b9b27566acbc0f3111397f8031920a93
ollieraikkonen <olliezeta9@gmail.com>
2022-06-11 10:08:53 +0100

    Crash Catcher - namespaces and globals

---
+++ plugins/crash_catcher_plugin/Main.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <FLHook.h>
+#include <plugin.h>
+
+namespace Plugins::CrashCatcher
+{
+	//! Global data for this plugin
+	struct Global final
+	{
+		ReturnCode returncode = ReturnCode::Default;
+
+		bool bPatchInstalled = false;
+		FARPROC fpOldTimingSeconds = 0;
+
+		HMODULE hModServerAC;
+		HMODULE hEngBase;
+		HMODULE hModContentAC;
+
+		std::map<uint, mstime> mapSaveTimes;
+	};
+
+	#define LOG_EXCEPTION_INTERNAL()								\
+	{																\
+		AddLogInternal("ERROR Exception in %s", __FUNCTION__);		\
+		AddExceptionInfoLog();										\
+	};
+} // namespace Plugins::CrashCatcher
+
+IMPORT struct CObject* __cdecl GetRoot(struct CObject const*);
