a30d5dfa28ccada5e96dda42e54b1cf2a6652959
ollieraikkonen <olliezeta9@gmail.com>
2022-06-18 18:37:08 +0100

    Kill Counter: Namespace and global struct

--- plugins/kill_counter_plugin/Main.cpp
+++ plugins/kill_counter_plugin/Main.cpp
@@ -2,36 +2,65 @@
 // Originally by ||KOS||Acid
 // https://sourceforge.net/projects/kosacid/files/
 
-#include <FLHook.h>
-#include <plugin.h>
+#include "Main.h"
 
-ReturnCode returncode = ReturnCode::Default;
-std::list<INISECTIONVALUE> lstRanks;
-
-EXPORT void UserCmd_Help(uint& iClientID, const std::wstring& wscParam)
+namespace Plugins::KillCounter
 {
-	PrintUserCmdText(iClientID, L"/kills <player name>");
-	PrintUserCmdText(iClientID, L"/kills$ <player id>");
-}
+	const std::unique_ptr<Global> global = std::make_unique<Global>();
 
-void UserCmd_Kills(uint iClientID, const std::wstring& wscParam)
-{
-	std::wstring wscClientID = GetParam(wscParam, ' ', 0);
-	int iNumKills;
-	std::wstring mainrank;
-	std::list<std::wstring> lstLines;
-	int count;
-	if (!wscClientID.length())
+	void UserCmd_Help(uint& iClientID, const std::wstring& wscParam)
 	{
-		std::wstring wscCharname = (const wchar_t*)Players.GetActiveCharacterName(iClientID);
-		HkReadCharFile(wscCharname, lstLines);
-		pub::Player::GetNumKills(iClientID, iNumKills);
+		PrintUserCmdText(iClientID, L"/kills <player name>");
+		PrintUserCmdText(iClientID, L"/kills$ <player id>");
+	}
+
+	void UserCmd_Kills(uint iClientID, const std::wstring& wscParam)
+	{
+		std::wstring wscClientID = GetParam(wscParam, ' ', 0);
+		int iNumKills;
+		std::wstring mainrank;
+		std::list<std::wstring> lstLines;
+		int count;
+		if (!wscClientID.length())
+		{
+			std::wstring wscCharname = (const wchar_t*)Players.GetActiveCharacterName(iClientID);
+			HkReadCharFile(wscCharname, lstLines);
+			pub::Player::GetNumKills(iClientID, iNumKills);
+			PrintUserCmdText(iClientID, L"PvP kills: %i", iNumKills);
+			for (auto& str : lstLines)
+			{
+				if (!ToLower((str)).find(L"ship_type_killed"))
+				{
+					uint iShipArchID = ToInt(GetParam(str, '=', 1).c_str());
+					count = ToInt(GetParam(str, ',', 1).c_str());
+					iNumKills += count;
+					Archetype::Ship* ship = Archetype::GetShip(iShipArchID);
+					if (!ship)
+						continue;
+					PrintUserCmdText(iClientID, L"NPC kills:  %s %i", HkGetWStringFromIDS(ship->iIdsName).c_str(), count);
+				}
+			}
+			int rank;
+			pub::Player::GetRank(iClientID, rank);
+			PrintUserCmdText(iClientID, L"Total kills: %i", iNumKills);
+			PrintUserCmdText(iClientID, L"Level: %i", rank);
+			return;
+		}
+
+		uint iClientIDPlayer = HkGetClientIdFromCharname(wscClientID);
+		if (iClientIDPlayer == -1)
+		{
+			PrintUserCmdText(iClientID, L"ERROR player not found");
+			return;
+		}
+		HkReadCharFile(wscClientID, lstLines);
+		pub::Player::GetNumKills(iClientIDPlayer, iNumKills);
 		PrintUserCmdText(iClientID, L"PvP kills: %i", iNumKills);
 		for (auto& str : lstLines)
 		{
 			if (!ToLower((str)).find(L"ship_type_killed"))
 			{
-				uint iShipArchID = ToInt(GetParam(str, '=', 1).c_str());
+				uint iShipArchID = ToInt(GetParam(str, '=', 1));
 				count = ToInt(GetParam(str, ',', 1).c_str());
 				iNumKills += count;
 				Archetype::Ship* ship = Archetype::GetShip(iShipArchID);
@@ -44,69 +73,40 @@ void UserCmd_Kills(uint iClientID, const std::wstring& wscParam)
 		pub::Player::GetRank(iClientID, rank);
 		PrintUserCmdText(iClientID, L"Total kills: %i", iNumKills);
 		PrintUserCmdText(iClientID, L"Level: %i", rank);
-		return;
 	}
 
-	uint iClientIDPlayer = HkGetClientIdFromCharname(wscClientID);
-	if (iClientIDPlayer == -1)
+	void __stdcall ShipDestroyed(DamageList** _dmg, DWORD** ecx, uint& iKill)
 	{
-		PrintUserCmdText(iClientID, L"ERROR player not found");
-		return;
-	}
-	HkReadCharFile(wscClientID, lstLines);
-	pub::Player::GetNumKills(iClientIDPlayer, iNumKills);
-	PrintUserCmdText(iClientID, L"PvP kills: %i", iNumKills);
-	for (auto& str : lstLines)
-	{
-		if (!ToLower((str)).find(L"ship_type_killed"))
+		if (iKill == 1)
 		{
-			uint iShipArchID = ToInt(GetParam(str, '=', 1));
-			count = ToInt(GetParam(str, ',', 1).c_str());
-			iNumKills += count;
-			Archetype::Ship* ship = Archetype::GetShip(iShipArchID);
-			if (!ship)
-				continue;
-			PrintUserCmdText(iClientID, L"NPC kills:  %s %i", HkGetWStringFromIDS(ship->iIdsName).c_str(), count);
-		}
-	}
-	int rank;
-	pub::Player::GetRank(iClientID, rank);
-	PrintUserCmdText(iClientID, L"Total kills: %i", iNumKills);
-	PrintUserCmdText(iClientID, L"Level: %i", rank);
-}
-
-void __stdcall ShipDestroyed(DamageList** _dmg, DWORD** ecx, uint& iKill)
-{
-	if (iKill == 1)
-	{
-		CShip* cship = (CShip*)(*ecx)[4];
-		uint iClientID = cship->GetOwnerPlayer();
-		DamageList* dmg = *_dmg;
-		if (iClientID)
-		{
-			if (!dmg->get_cause())
-				dmg = &ClientInfo[iClientID].dmgLast;
-			uint iClientIDKiller = HkGetClientIDByShip(dmg->get_inflictor_id());
-			if (iClientIDKiller && (iClientID != iClientIDKiller))
+			CShip* cship = (CShip*)(*ecx)[4];
+			uint iClientID = cship->GetOwnerPlayer();
+			DamageList* dmg = *_dmg;
+			if (iClientID)
 			{
-				int iNumKills;
-				pub::Player::GetNumKills(iClientIDKiller, iNumKills);
-				iNumKills++;
-				pub::Player::SetNumKills(iClientIDKiller, iNumKills);
+				if (!dmg->get_cause())
+					dmg = &ClientInfo[iClientID].dmgLast;
+				uint iClientIDKiller = HkGetClientIDByShip(dmg->get_inflictor_id());
+				if (iClientIDKiller && (iClientID != iClientIDKiller))
+				{
+					int iNumKills;
+					pub::Player::GetNumKills(iClientIDKiller, iNumKills);
+					iNumKills++;
+					pub::Player::SetNumKills(iClientIDKiller, iNumKills);
+				}
 			}
 		}
 	}
-}
 
-USERCMD UserCmds[] = {
-	{ L"/kills", UserCmd_Kills },
-};
+	USERCMD UserCmds[] = {
+	    {L"/kills", UserCmd_Kills},
+	};
 
-// Process user input
-bool UserCmd_Process(uint& iClientID, const std::wstring& wscCmd)
-{
-	DefaultUserCommandHandling(iClientID, wscCmd, UserCmds, returncode);
-}
+	// Process user input
+	bool UserCmd_Process(uint& iClientID, const std::wstring& wscCmd) { DefaultUserCommandHandling(iClientID, wscCmd, UserCmds, global->returncode); }
+} // namespace Plugins::KillCounter
+
+using namespace Plugins::KillCounter;
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
@@ -119,7 +119,7 @@ extern "C" EXPORT void ExportPluginInfo(PluginInfo* pi)
 	pi->shortName("killcounter");
 	pi->mayPause(false);
 	pi->mayUnload(true);
-	pi->returnCode(&returncode);
+	pi->returnCode(&global->returncode);
 	pi->versionMajor(PluginMajorVersion::VERSION_04);
 	pi->versionMinor(PluginMinorVersion::VERSION_00);
 	pi->emplaceHook(HookedCall::FLHook__UserCommand__Process, &UserCmd_Process);
