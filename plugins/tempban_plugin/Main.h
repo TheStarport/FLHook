447a60b2f4ac41e1195fe9a180a39537b59a090c
ollieraikkonen <olliezeta9@gmail.com>
2022-06-19 14:27:06 +0100

    Tempban: Global and namespaces

--- plugins/tempban_plugin/Main.h
+++ plugins/tempban_plugin/Main.h
@@ -2,20 +2,30 @@
 
 #include <FLHook.h>
 #include <plugin.h>
-#include <utility>
 
-struct TEMPBAN_INFO
+namespace Plugins::Tempban
 {
-	std::wstring wscID;
-	mstime banstart;
-	mstime banduration;
-};
+	struct TEMPBAN_INFO
+	{
+		std::wstring wscID;
+		mstime banstart;
+		mstime banduration;
+	};
 
-class TempBanCommunicator : public PluginCommunicator
-{
-  public:
-	inline static const char* pluginName = "TempBan Plugin by w0dk4";
-	explicit TempBanCommunicator(std::string plug);
+	class TempBanCommunicator : public PluginCommunicator
+	{
+	  public:
+		inline static const char* pluginName = "TempBan Plugin by w0dk4";
+		explicit TempBanCommunicator(std::string plug);
 
-	HK_ERROR PluginCall(TempBan, const std::wstring& characterName, uint duration);
-};
+		HK_ERROR PluginCall(TempBan, const std::wstring& characterName, uint duration);
+	};
+
+	struct Global final
+	{
+		std::list<TEMPBAN_INFO> TempBans;
+
+		ReturnCode returncode = ReturnCode::Default;
+		TempBanCommunicator* communicator = nullptr;
+	};
+}
