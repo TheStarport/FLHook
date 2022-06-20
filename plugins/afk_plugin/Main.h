a63b6fb9e0f8fce635d36c54c9d779726d4177fa
ollieraikkonen <olliezeta9@gmail.com>
2022-06-20 15:24:19 +0100

    Linting for Afk

--- plugins/afk_plugin/Main.h
+++ plugins/afk_plugin/Main.h
@@ -4,14 +4,14 @@
 #include <FLHook.hpp>
 #include <plugin.h>
 
-namespace Plugins::AFK
+namespace Plugins::Afk
 {
 	//! Global data for this plugin
 	struct Global final
 	{
 		// Other fields
-		ReturnCode returncode = ReturnCode::Default;
+		ReturnCode returnCode = ReturnCode::Default;
 
-		std::set<uint> afks;
+		std::vector<uint> awayClients;
 	};
 }
