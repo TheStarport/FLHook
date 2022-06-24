71f8d332e9b36fa60249fa7abcfd018467ff947e
Lazrius <lazdisco@gmail.com>
2022-06-24 19:39:43 +0100

    port purchase restrictions

--- plugins/purchase_restrictions_plugin/Main.h
+++ plugins/purchase_restrictions_plugin/Main.h
@@ -3,44 +3,37 @@
 #include <FLHook.hpp>
 #include <plugin.h>
 
-ReturnCode returncode = ReturnCode::Default;
-
-/// If true then an ID is used to check for valid IDs
-static bool set_bCheckIDRestrictions = false;
-
-/// If true then an ID is used to check for valid IDs
-static bool set_bEnforceIDRestrictions = false;
-
-/// Message if ship purchase denied
-static std::wstring set_wscShipPurchaseDenied = L"";
-
-/// Message if good purchase denied
-static std::wstring set_wscGoodPurchaseDenied = L"";
-
-/// list of items that we log transfers for
-static std::map<uint, std::string> set_mapItemsOfInterest;
-
-/** A map of baseid to goods that are not allowed to be bought */
-static std::multimap<uint, uint> set_mapNoBuy;
-typedef std::multimap<uint, uint, std::less<uint>>::value_type mapnobuy_map_pair_t;
-typedef std::multimap<uint, uint, std::less<uint>>::iterator mapnobuy_map_iter_t;
-
-/** A map of goods that are not allowed to be bought without the correct item.
- */
-static std::multimap<uint, uint> set_mapGoodItemRestrictions;
-typedef std::multimap<uint, uint, std::less<uint>>::value_type mapGoodItemRestrictions_map_pair_t;
-typedef std::multimap<uint, uint, std::less<uint>>::iterator mapGoodItemRestrictions_map_iter_t;
-
-/** A map of ship that are not allowed to be bought without the correct item. */
-static std::multimap<uint, uint> set_mapShipItemRestrictions;
-typedef std::multimap<uint, uint, std::less<uint>>::value_type mapShipItemRestrictions_map_pair_t;
-typedef std::multimap<uint, uint, std::less<uint>>::iterator mapShipItemRestrictions_map_iter_t;
-
-// List of connected clients.
-struct INFO
+namespace Plugins::PurchaseRestrictions
 {
-	INFO() : bSuppressBuy(false) {}
+	struct Config : Reflectable
+	{
+		std::string File() override { return "flhook_plugins/purchaseRestrictions.json"; }
+		bool checkItemRestrictions = false;
+		bool enforceItemRestrictions = false;
+		std::wstring shipPurchaseDenied = L"You are not authorized to buy this ship.";
+		std::wstring goodPurchaseDenied = L"You are not authorized to buy this item.";
 
-	bool bSuppressBuy;
-};
-static std::map<uint, INFO> mapInfo;
+		//! Items that we log transfers for
+		std::vector<std::string> itemsOfInterest;
+
+		//! Items that cannot be bought at all.
+		std::vector<std::string> unbuyableItems;
+
+		//! Items that can only be bought with a certain item equipped (item, [ equippedItemsThatAllowPurchase ])
+		std::map<std::string, std::vector<std::string>> goodItemRestrictions;
+		//! Ships that can only be bought with a certain item equipped (ship, [ equippedItemsThatAllowPurchase ])
+		std::map<std::string, std::vector<std::string>> shipItemRestrictions;
+	};
+
+	struct Global
+	{
+		Config config;
+		ReturnCode returnCode = ReturnCode::Default;
+		std::map<ClientId, bool> clientSuppressBuy;
+
+		std::vector<uint> unbuyableItemsHashed;
+		std::map<uint, std::string> itemsOfInterestHashed;
+		std::map<uint, std::vector<uint>> goodItemRestrictionsHashed;
+		std::map<uint, std::vector<uint>> shipItemRestrictionsHashed;
+	};
+} // namespace Plugins::PurchaseRestrictions
