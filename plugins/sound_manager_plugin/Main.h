e647b09ba9b62f075e3439d68a18a60032b53a61
oliverpechey <olliezeta9@gmail.com>
2022-07-10 12:13:14 +0100

    Sound Manager, make json file name consistent with .dll name

--- plugins/sound_manager_plugin/Main.h
+++ plugins/sound_manager_plugin/Main.h
@@ -9,7 +9,7 @@ namespace Plugins::SoundManager
 	struct Config final : Reflectable
 	{
 		//! This json file should contain a "sounds" array containing a list of sounds for e.g. dx_s032a_01a01_hvis_xtr_1
-		std::string File() override { return "flhook_plugins/soundmanager.json"; }
+		std::string File() override { return "flhook_plugins/sound_manager.json"; }
 
 		//! A vector of sounds in their Freelancer name format
 		std::vector<std::string> sounds;
