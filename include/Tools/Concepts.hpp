e305e2e31f20ff793b0ac7a1637a12b5249426d5
MrNen <sjduttenhefer@gmail.com>
2023-02-03 21:46:07 -0600

    Formatting of util files, grammar fixes on some bank error strings. Temporary placeholder text to tell user to use /bank for help commands untill multi-line printusercmd functionaltiy is added.

--- include/Tools/Concepts.hpp
+++ include/Tools/Concepts.hpp
@@ -2,5 +2,5 @@
 #include <string>
 #include <type_traits>
 
-template <typename T>
+template<typename T>
 concept StringRestriction = std::is_same<std::string, T>::value || std::is_same<std::wstring, T>::value;
--- include/Tools/Hk.hpp
+++ include/Tools/Hk.hpp
@@ -30,7 +30,7 @@ namespace Hk
 	{
 		std::chrono::seconds Unix();
 		std::chrono::milliseconds UnixMilliseconds();
-		
+
 		DLL uint GetUnix();
 
 		template<typename T>
@@ -311,8 +311,8 @@ namespace Hk
 	namespace ZoneUtilities
 	{
 		DLL void ReadUniverse(std::multimap<uint, LootableZone, std::less<>>* zones);
-		DLL void ReadLootableZone(std::multimap<uint, LootableZone, std::less<>>& zones, const std::string& systemNick,
-		    const std::string& defaultZoneNick, const std::string& file);
+		DLL void ReadLootableZone(
+		    std::multimap<uint, LootableZone, std::less<>>& zones, const std::string& systemNick, const std::string& defaultZoneNick, const std::string& file);
 		DLL void ReadSystemLootableZones(std::multimap<uint, LootableZone, std::less<>>& zones, const std::string& systemNick, const std::string& file);
 		DLL void ReadSystemZones(std::multimap<uint, LootableZone, std::less<>>& zones, const std::string& systemNick, const std::string& file);
 		DLL bool InZone(uint systemId, const Vector& pos, Zone& rlz);
