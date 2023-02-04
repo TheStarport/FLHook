72376a5259542661653088b7ba1ef3b790c48755
MrNen <sjduttenhefer@gmail.com>
2023-02-04 12:21:11 -0600

    Reworked PrintUserCmd to accept the new line char to format strings instead of having to call it for every new line you want to print. Added another concept for variadic templates.

--- include/Tools/Concepts.hpp
+++ include/Tools/Concepts.hpp
@@ -3,4 +3,7 @@
 #include <type_traits>
 
 template<typename T>
-concept StringRestriction = std::is_same<std::string, T>::value || std::is_same<std::wstring, T>::value;
+concept StringRestriction = std::is_same_v<std::string, T> || std::is_same_v<std::wstring, T>;
+
+template<typename ... T>
+concept AtLeastOne = (sizeof...(T) > 0);
