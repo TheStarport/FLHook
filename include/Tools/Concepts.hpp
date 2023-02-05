7f6c64bbada5e4b55e27dfac3eff05cf0e2c7e90
Laz <lazdisco@gmail.com>
2023-02-05 14:59:13 +0000

    feat: allow save game location to be moddified

--- include/Tools/Concepts.hpp
+++ include/Tools/Concepts.hpp
@@ -7,3 +7,9 @@ concept StringRestriction = std::is_same_v<std::string, T> || std::is_same_v<std
 
 template<typename ... T>
 concept AtLeastOne = (sizeof...(T) > 0);
+
+template<typename Base, typename Derrived>
+concept IsDerivedFrom = std::derived_from<Derrived, Base>;
+
+template<typename Base, typename Derrived>
+concept IsBaseOf = std::is_base_of_v<Base, Derrived>;
