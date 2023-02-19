1df2f7388a7d657e47ce5577c2e15626f68f4119
Laz <lazdisco@gmail.com>
2023-02-19 15:00:22 +0000

    feat: add serializer validation allowing fields to be validated against invalid values

--- include/Tools/Concepts.hpp
+++ include/Tools/Concepts.hpp
@@ -8,6 +8,15 @@ concept StringRestriction = std::is_same_v<std::string, T> || std::is_same_v<std
 template<typename ... T>
 concept AtLeastOne = (sizeof...(T) > 0);
 
+template<typename T>
+concept IsIntegral = std::is_integral_v<T>;
+
+template<typename T>
+concept IsSignedIntegral = IsIntegral<T> && std::is_signed_v<T>;
+
+template<typename T>
+concept IsUnsignedIntegral = IsIntegral<T> && !IsSignedIntegral<T>;
+
 template<typename Base, typename Derrived>
 concept IsDerivedFrom = std::derived_from<Derrived, Base>;
 
