5d04fcafb2bff561c5ce4e37d31a50b462d94f54
Laz <lazdisco@gmail.com>
2023-06-11 12:05:19 +0100

    update clang format and comments for Action.hpp

--- include/Exceptions/Action.hpp
+++ include/Exceptions/Action.hpp
@@ -2,55 +2,79 @@
 
 #include "GameException.hpp"
 
-template<typename Ret>
-    requires RequireParamterlessConstructor<Ret> || IsSameAs<Ret, void>
+/*
+   \brief An action class is a wrapper around an internal function that may need to have its error handled gracefully.
+	In the event that something goes wrong, there are multiple ways to handle, including return a default value,
+	run an lambda function with an error code and message paramaters, or throw an exception if the result is not desired in the event of error.
+
+   \note Internally the action class uses cpp::result, a library based on the C++23 standard library 'result_of' function.
+   \note The specified type can only be one that has a default constructor.
+
+   \tparam Ret The return type of the calling function
+*/
+template <typename Ret>
+	requires RequireParamterlessConstructor<Ret> || IsSameAs<Ret, void>
 class Action
 {
-	cpp::result<Ret, Error> result;
+		cpp::result<Ret, Error> result;
 
-  public:
-	// ReSharper disable once CppNonExplicitConvertingConstructor
-	Action(cpp::result<Ret, Error> res) : result(res) {}
+	public:
+		// ReSharper disable once CppNonExplicitConvertingConstructor
+		Action(cpp::result<Ret, Error> res) : result(res) {}
 
-	//Will check to see if the function has returned an error and throws a GameException if so, you may handle the error yourself by
-	// passing in a lambda in the arguments or let Flhook's exception handler do it for you by passing in no arguments.
-	Ret Handle(const std::optional<std::function<void(Error, std::wstring_view)>>& func = std::nullopt)
-	{
-		constexpr bool returnTypeIsVoid = std::is_same_v<Ret, void>;
+		/*
+			\brief Return the value of the called function that is of type Ret. If no param is provided, and the function errored when called
+			this will cause a GameException to be thrown. If a std::function of type void(Error, std::wstring_view) is provided, this will be called
+			instead of an exception being raised.
 
-		if (result.has_error())
+			\param func A lambda that allowed errors to be handled gracefully rather than throwing an exception. It will be called in the event of an error
+		   being present.
+			\exception GameException A game exception is raised when Handle is called without a parameter and the called function errors.
+		*/
+		Ret Handle(const std::optional<std::function<void(Error, std::wstring_view)>>& func = std::nullopt)
 		{
-			if (func == std::nullopt)
+			constexpr bool returnTypeIsVoid = std::is_same_v<Ret, void>;
+
+			if (result.has_error())
 			{
-				throw GameException(L"An error has occurred.", result.error());
+				if (func == std::nullopt)
+				{
+					throw GameException(L"An error has occurred.", result.error());
+				}
+
+				func(result.error(), L"An error has occurred.");
+				if constexpr (returnTypeIsVoid)
+				{
+					return;
+				}
+				else
+				{
+					return Ret();
+				}
 			}
 
-			func(result.error(), L"An error has occurred.");
-			if constexpr (returnTypeIsVoid)
+			return result.value();
+		}
+
+		/*
+			\brief Return the value of the underlying return type. If the called function errored, the default constructor for the specified type will be called
+		   instead.
+		*/
+		Ret Unwrap()
+		{
+			if constexpr (std::is_same_v<Ret, void>)
 			{
-				return;
+				throw UnsupportedException();
 			}
 			else
 			{
-				return Ret();
+				return result.value_or(Ret());
 			}
 		}
 
-		return result.value();
-	}
-	//Simple returns the result value without the error, will return a default value if an error is present.
-	Ret Unwrap()
-	{
-		if constexpr (std::is_same_v<Ret, void>)
-		{
-			throw UnsupportedException();
-		}
-		else
-		{
-			return result.value_or(Ret());	
-		}
-	}
-
-	//Returns a cpp::result of the return type and Error.
-	const cpp::result<Ret, Error>& Raw() { return result; }
+		/*
+			\brief Returns the underlying internal error object. This is not recommended for standard usage and consumption of functions
+			and is designed to only be used internally.
+		*/
+		const cpp::result<Ret, Error>& Raw() { return result; }
 };
