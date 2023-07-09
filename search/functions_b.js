f723101652f0d5009214d69da54975f7c0e28fb8
oliverpechey <40522943+oliverpechey@users.noreply.github.com>
2023-07-09 21:00:11 +0000

    Deploying to gh-pages from @ TheStarport/FLHook@cf2daf49face299b2e394e68ad712aa60b16d260 🚀

--- search/functions_b.js
+++ search/functions_b.js
@@ -4,6 +4,6 @@ var searchData=
   ['kickreason_1',['KickReason',['../namespace_hk_1_1_player.html#a3f4fde0fa8ff787a6ee9bff4859ab0a2',1,'Hk::Player']]],
   ['kill_2',['Kill',['../namespace_hk_1_1_player.html#a63220ebb4f78eb38c4c7b93d5b6cf081',1,'Hk::Player']]],
   ['killban_3',['KillBan',['../group___anti_jump_disconnect.html#ga5a5e78d5b5b599a416b480dee6743110',1,'Plugins::AntiJumpDisconnect']]],
-  ['killmessage_4',['KillMessage',['../struct_plugins_1_1_kill_tracker_1_1_kill_message.html#aad6c0ef4b128264c52b202d07766e591',1,'Plugins::KillTracker::KillMessage::KillMessage(int number_, std::wstring message_)'],['../struct_plugins_1_1_kill_tracker_1_1_kill_message.html#a71fe556f0cd87d6f7c937aba35f49d62',1,'Plugins::KillTracker::KillMessage::KillMessage()']]],
+  ['killmessage_4',['KillMessage',['../struct_plugins_1_1_kill_tracker_1_1_kill_message.html#a41250ec5524241554877d8c7b86efc7d',1,'Plugins::KillTracker::KillMessage::KillMessage(int numberParam, std::wstring const &amp;messageParam)'],['../struct_plugins_1_1_kill_tracker_1_1_kill_message.html#ad57ab9e7e07cfeac8dad29ffd204fa5e',1,'Plugins::KillTracker::KillMessage::KillMessage()=default']]],
   ['killsyminfo_5',['KillSymInfo',['../xtrace_8cpp.html#a37868837c9234b58eb7d828b62b10f3a',1,'KillSymInfo():&#160;xtrace.cpp'],['../xtrace_8h.html#a37868837c9234b58eb7d828b62b10f3a',1,'KillSymInfo():&#160;xtrace.cpp']]]
 ];
--- struct_plugins_1_1_kill_tracker_1_1_kill_message.js
+++ struct_plugins_1_1_kill_tracker_1_1_kill_message.js
@@ -1,7 +1,7 @@
 var struct_plugins_1_1_kill_tracker_1_1_kill_message =
 [
-    [ "KillMessage", "struct_plugins_1_1_kill_tracker_1_1_kill_message.html#aad6c0ef4b128264c52b202d07766e591", null ],
-    [ "KillMessage", "struct_plugins_1_1_kill_tracker_1_1_kill_message.html#a71fe556f0cd87d6f7c937aba35f49d62", null ],
+    [ "KillMessage", "struct_plugins_1_1_kill_tracker_1_1_kill_message.html#a41250ec5524241554877d8c7b86efc7d", null ],
+    [ "KillMessage", "struct_plugins_1_1_kill_tracker_1_1_kill_message.html#ad57ab9e7e07cfeac8dad29ffd204fa5e", null ],
     [ "message", "struct_plugins_1_1_kill_tracker_1_1_kill_message.html#a069b13af023f4a7fbb2de568567b6ce9", null ],
     [ "number", "struct_plugins_1_1_kill_tracker_1_1_kill_message.html#a5ee2a57cc68d814fff2679f7fccf07d0", null ]
 ];
