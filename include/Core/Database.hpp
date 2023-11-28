59a2c117e1976159e47beaf385be9c020bb32592
MrNen <sjduttenhefer@gmail.com>
2023-11-28 17:39:46 -0600

    added mongo-cxx-driver to vcpkg. TODO: Figure why the vcpkg is failing to build mongocxx

---
+++ include/Core/Database.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+#include "bsoncxx/builder/basic/document.hpp"
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+
+
+class Database
+{
+      
+        Database();
+        ~Database();
+
+
+};
