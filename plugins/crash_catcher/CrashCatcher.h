b0946ef1089da307f52facdce54d573cfcf8c3ec
Rupert Walker <RupertCW@gmail.com>
2024-01-22 13:25:27 +0000

    Add the offsets back and into a config for CrashCatcher.cpp

--- plugins/crash_catcher/CrashCatcher.h
+++ plugins/crash_catcher/CrashCatcher.h
@@ -5,6 +5,20 @@
 
 namespace Plugins::CrashCatcher
 {
+
+	//! Config data for this plugin
+	struct Config final : Reflectable
+	{
+		std::string File() override { return "config/crashcatcher.json"; }
+
+		//! Sets the maximum distance at which NPCs become visible as per scanner settings, a value of 0 will disable this override.
+		float npcVisibilityDistance = 6500.f;
+		//! Sets the distnace at which NPCs will despawn, a value of 0 will disable this override.
+		float npcPersistDistance = 6500.f;
+		//! Sets the distance at which NPCs will spawn, a value of 0 will disable this override.
+		float npcSpawnDistance = 6500.f;
+	};
+
 	//! Global data for this plugin
 	struct Global final
 	{
@@ -18,12 +32,14 @@ namespace Plugins::CrashCatcher
 		HMODULE hModContentAC;
 
 		std::map<uint, mstime> mapSaveTimes;
+		//! Contains config data defined above
+		std::unique_ptr<Config> config = nullptr;
 	};
 
-	#define LOG_EXCEPTION_INTERNAL()								\
-	{																\
-		AddLogInternal("ERROR Exception in %s", __FUNCTION__);		\
-		AddExceptionInfoLog();										\
+#define LOG_EXCEPTION_INTERNAL()                               \
+	{                                                          \
+		AddLogInternal("ERROR Exception in %s", __FUNCTION__); \
+		AddExceptionInfoLog();                                 \
 	};
 } // namespace Plugins::CrashCatcher
 
