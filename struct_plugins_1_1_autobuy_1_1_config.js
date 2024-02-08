f0fd699aaa6d90f1768aa54059cf1e3354199423
oliverpechey <40522943+oliverpechey@users.noreply.github.com>
2024-02-08 18:09:30 +0000

    Deploying to gh-pages from @ TheStarport/FLHook@4832a27845693883f1e7a86597c0a93436177097 🚀

--- struct_plugins_1_1_autobuy_1_1_config.js
+++ struct_plugins_1_1_autobuy_1_1_config.js
@@ -1,7 +1,7 @@
 var struct_plugins_1_1_autobuy_1_1_config =
 [
     [ "File", "struct_plugins_1_1_autobuy_1_1_config.html#a460e769640a7ae8b27890f19256dc35b", null ],
-    [ "iniPaths", "struct_plugins_1_1_autobuy_1_1_config.html#a68289fb41ce3be97ca0582ffe5882d34", null ],
+    [ "ammoIniPaths", "struct_plugins_1_1_autobuy_1_1_config.html#a31780a0ae519977a0aeac07ae36be641", null ],
     [ "nanobot_nickname", "struct_plugins_1_1_autobuy_1_1_config.html#a030d7ab65fa4292ec967b262f5d706b6", null ],
     [ "shield_battery_nickname", "struct_plugins_1_1_autobuy_1_1_config.html#a890957fd9eb1736b48e5cefa73305cb7", null ]
 ];
