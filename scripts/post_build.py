355f6900694dadc2ff7955392b4f0510e27d58d7
Laz <lazdisco@gmail.com>
2024-05-25 13:14:33 +0100

    build: ensure that duplicate files are not copied in post-build

--- scripts/post_build.py
+++ scripts/post_build.py
@@ -1,9 +1,13 @@
-from shutil import copy2, copytree, rmtree
+from shutil import copy2
+from pathlib import Path
 import os
 from argparse import ArgumentParser, ArgumentError
 
-copy_path = os.environ.get("FLHOOK_COPY_PATH")
+copy_path = os.environ.get("FLHOOK_COPY_PATH") or ""
+if copy_path == "":
+    raise EnvironmentError("FLHOOK_COPY_PATH environment variable is missing.")
 
+copied_files = []
 bin_dir = None
 
 if __name__ != "__main__":
@@ -16,13 +20,26 @@ args = parser.parse_args()
 if not os.path.isdir(args.bin):
     raise ArgumentError(argument=args.bin, message="Provided Bin Directory was not a valid directory")
 
+
+def process_folder(files, destination):
+    for src in files:
+        # Don't copy the same file multiple times
+        file_name = Path(src).stem
+        if file_name in copied_files:
+            continue
+
+        source_path = str(os.path.join(args.bin, src))
+        print("Copying {0} to {1}".format(src, destination))
+        if os.path.isdir(source_path):
+            out_dir = os.path.join(destination, file_name)
+            if not os.path.exists(out_dir):
+                os.mkdir(out_dir)
+
+            process_folder([os.path.join(source_path, d) for d in os.listdir(source_path)], out_dir)
+        else:
+            copy2(source_path, destination)
+            copied_files.append(file_name)
+
+
 print("Copying files from {0} to {1}".format(args.bin, copy_path))
-dirs = [d for d in os.listdir(args.bin)]
-for src in dirs:
-    joined = os.path.join(args.bin, src)
-    dest = os.path.join(copy_path, src)
-    print("Copying {0} to {1}".format(src, dest))
-    if os.path.isdir(joined):
-        copytree(joined, dest, False, None, dirs_exist_ok=True)
-    else:
-        copy2(joined, copy_path)
+process_folder([os.path.join(args.bin, d) for d in os.listdir(args.bin)], copy_path)
