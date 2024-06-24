b9be1fdbb4a25e75073d204d1d54b61ba5380d8f
Laz <lazdisco@gmail.com>
2024-06-24 16:31:15 +0100

    chore: expose ForEachGroupMember inside of GroupId

--- include/API/Types/GroupId.hpp
+++ include/API/Types/GroupId.hpp
@@ -4,17 +4,6 @@ class DLL GroupId final
 {
         uint value = 0;
 
-        Action<void, Error> ForEachGroupMember(const std::function<std::optional<Error>(ClientId client)>& func, bool stopIfErr = true) const;
-
-        template <typename T, T>
-        struct Proxy;
-
-        template <typename T, typename R, typename... Args, R (T::*MemberFunc)(Args...)>
-        struct Proxy<R (T::*)(Args...), MemberFunc>
-        {
-                static R Call(T& obj, Args&&... args) { return (obj.*MemberFunc)(std::forward<Args>(args)...); }
-        };
-
     public:
         explicit GroupId(const uint val) : value(val) {}
         explicit GroupId() = default;
@@ -31,14 +20,7 @@ class DLL GroupId final
 
         Action<std::vector<ClientId>, Error> GetGroupMembers() const;
         Action<uint, Error> GetGroupSize() const;
-
-        template <typename FunctionPtr, typename... Args>
-            requires std::is_same_v<typename MemberFunctionPointerClassType<FunctionPtr>::type, ClientId>
-        Action<void, Error> GroupAction(FunctionPtr ptr, bool stopIfErr, Args... args) const
-        {
-            return ForEachGroupMember([ptr, args](ClientId client) { Proxy<void (ClientId::*)(), ptr>::Call(client, args...); }, stopIfErr);
-        }
-
+        Action<void, Error> ForEachGroupMember(const std::function<std::optional<Error>(ClientId client)>& func, bool stopIfErr = true) const;
         Action<void, Error> InviteMember(ClientId client);
         Action<void, Error> AddMember(ClientId client);
 };
