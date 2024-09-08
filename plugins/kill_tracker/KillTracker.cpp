363881254fb88a48ffd340a6401478b0dda6c917
Aingar <mierzei@gmail.com>
2024-09-08 18:27:17 +0200

    feat: Partial KillTracker implementation

--- plugins/kill_tracker/KillTracker.cpp
+++ plugins/kill_tracker/KillTracker.cpp
@@ -6,8 +6,64 @@
 
 namespace Plugins
 {
+    void KillTrackerPlugin::ClearDamageTaken(const ClientId client) { damageArray[client.GetValue()].fill({ 0.0f, 0.0f }); }
+    void KillTrackerPlugin::ClearDamageDone(const ClientId client, const bool fullReset)
+    {
+        for (int i = 1; i <= MaxClientId; i++)
+        {
+            auto& [currDamage, lastUndockDamage] = damageArray[i][client.GetValue()];
+            if (fullReset)
+            {
+                lastUndockDamage = 0.0f;
+            }
+            else
+            {
+                lastUndockDamage = currDamage;
+            }
+            currDamage = 0.0f;
+        }
+    }
 
-}
+    void KillTrackerPlugin::OnCharacterSelectAfter(ClientId client)
+    {
+        ClearDamageTaken(client);
+        ClearDamageDone(client, true);
+    }
+    void KillTrackerPlugin::OnDisconnectAfter(ClientId client, EFLConnection connection)
+    {
+        ClearDamageTaken(client);
+        ClearDamageDone(client, true);
+    }
+    void KillTrackerPlugin::OnClearClientInfo(ClientId client)
+    {
+        ClearDamageTaken(client);
+        ClearDamageDone(client, true);
+    }
+    void KillTrackerPlugin::OnPlayerLaunchAfter(ClientId client, ShipId ship)
+    {
+        ClearDamageTaken(client);
+        ClearDamageDone(client, false);
+    }
+
+    void KillTrackerPlugin::OnShipHullDmg(Ship* ship, float& damage, DamageList* dmgList)
+    {
+        if (!dmgList->inflictorPlayerId)
+        {
+            return;
+        }
+
+        if (const auto shipOwnerPlayer = ship->cship()->ownerPlayer; shipOwnerPlayer && shipOwnerPlayer != dmgList->inflictorPlayerId && damage > 0.0f)
+        {
+            damageArray[shipOwnerPlayer][dmgList->inflictorPlayerId].currDamage += damage;
+        }
+    }
+    void KillTrackerPlugin::OnSendDeathMessage(ClientId killer, ClientId victim, SystemId system, std::wstring_view msg)
+    {
+        // TODO: Print death message, supress core death message printing.
+    }
+} // namespace Plugins
+
+using namespace Plugins;
 
 DefaultDllMain();
 
--- plugins/kill_tracker/KillTracker.hpp
+++ plugins/kill_tracker/KillTracker.hpp
@@ -4,9 +4,32 @@ namespace Plugins
 {
     class KillTrackerPlugin final : public Plugin
     {
-        struct Config final
-        {
+            struct Config final
+            {
+                    uint numberOfListedKillers = 3;
+                    float deathBroadcastRange = 15000;
+                    uint minimumAssistancePercentage = 5;
+            };
 
-        }
-	}
-} // namespace Plugins::KillTracker
+            struct DamageDone
+            {
+                    float currDamage = 0.0f;
+                    float lastUndockDamage = 0.0f;
+            };
+
+            Config config;
+            std::array<std::array<DamageDone, MaxClientId + 1>, MaxClientId + 1> damageArray;
+
+            void ClearDamageTaken(ClientId client);
+            void ClearDamageDone(ClientId client, bool fullReset);
+            void OnCharacterSelectAfter(ClientId client) override;
+            void OnDisconnectAfter(ClientId client, EFLConnection connection) override;
+            void OnClearClientInfo(ClientId client) override;
+            void OnShipHullDmg(Ship* ship, float& damage, DamageList* dmgList) override;
+            void OnSendDeathMessage(ClientId killer, ClientId victim, SystemId system, std::wstring_view msg) override;
+            void OnPlayerLaunchAfter(ClientId client, ShipId ship) override;
+
+        public:
+            explicit KillTrackerPlugin(const PluginInfo& info);
+    };
+} // namespace Plugins
